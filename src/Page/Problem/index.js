f46aec6a7a2ba209cb68d402d13b07f955308085
soso01 <6125024@gmail.com>
2020-04-11 23:01:19 +0900

    problem page

--- src/Page/Problem/index.js
+++ src/Page/Problem/index.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import Layout from '../../Component/Layout'
+import React from "react"
+import Layout from "../../Component/Layout"
 
-const Problem = ({match}) => {
-    return (
-        <Layout>
-            문제
-            {match.params.id}
-        </Layout>
-    )
+import "./Problem.scss"
+
+const Problem = ({ match }) => {
+  return (
+    <Layout>
+      <div className="">
+        문제
+        {match.params.id}
+        <div>표</div>
+        <div>문제</div>
+        <div>입력</div>
+        <div>출력</div>
+        <div>
+          <div>예제입력1</div>
+          <div>예제출력1</div>
+        </div>
+        <div>
+          <div>예제입력2</div>
+          <div>예제출력2</div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Problem
