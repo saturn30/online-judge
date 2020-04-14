0077754df70f9f0356540e4b0ea26eb2ef4649fc
soso01 <6125024@gmail.com>
2020-04-14 21:03:12 +0900

    css login page

--- src/Page/Login/index.js
+++ src/Page/Login/index.js
@@ -6,23 +6,43 @@ import "./Login.scss"
 const Login = () => {
   return (
     <Layout>
-      <div className="login-box">
-        <div className="login-item">로그인</div>
-        <div className="login-item">
-          <div className="login-input">
-            <div className="login-label">아이디</div>
-            <input type="text" name="name" />
+      <div className="columns">
+        <div className="column is-offset-3-desktop is-half-desktop is-offset-2-tablet is-8-tablet">
+          <div className="box" style={{ marginTop: 100 }}>
+            <div className="container">
+              <p className="has-text-centered login-title">
+                <strong>로그인</strong>
+              </p>
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">ID : </label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <p className="control">
+                      <input className="input" type="text" readonly />
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                  <label className="label">PW : </label>
+                </div>
+                <div className="field-body">
+                  <div className="field">
+                    <p className="control">
+                      <input className="input" type="password" readonly />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="login-buttons">
+              <button class="button is-primary">회원가입</button>
+              <button class="button is-link">로그인</button>
+            </div>
           </div>
-        </div>
-        <div className="login-item">
-          <div className="login-input">
-            <div className="login-label">비밀번호</div>
-            <input type="text" name="name" />
-          </div>
-        </div>
-        <div className="login-item">
-          <button>회원가입</button>
-          <button>로그인</button>
         </div>
       </div>
     </Layout>
